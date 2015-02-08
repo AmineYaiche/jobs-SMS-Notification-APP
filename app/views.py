d6f280e2760c202fabad5162339c5bd13daf3885
Amine YAICHE <yaiche.amin@gmail.com>
2015-02-08 23:09:43 +0100

    add twilio feature

--- app/views.py
+++ app/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from app.models import job , contact
+import requests
+from bitly.models import Bittle
+from twilio.rest import TwilioRestClient
 
-# Create your views here.
+def bitlify(url):
+	return Bittle.objects.bitlify(url).shortUrl
+
+
+def fetch(url):
+	r = requests.get(url)
+	parsed = r.json()
+	jobs = parsed["objects"]
+	job.objects.filter().delete()
+
+	i = 0
+
+	for j in jobs:
+		i+=1
+		if i == 20: break
+		elem = job()
+		elem.title= str(j['title'].encode("utf-8"))
+		elem.recruiter=str(j['recruiter'].encode("utf-8"))
+		elem.link = bitlify(j['link'])
+		elem.save()
+	return job.objects.all()[:4]
+
+
+
+def send(jobs):
+	
+	contacts = contact.objects.all()
+
+	for j in jobs:
+		for c in contacts:
+			message = "Hey {}, {} has published a new job {} {}".format(c.first_name , j.recruiter , j.title  , j.link)
+			account_sid = "ACf71c0db239a537d8f4647ddabae65d12"
+			auth_token = "86ca1e9472dd730c26552aea7cf44fa5"
+			client = TwilioRestClient(account_sid , auth_token)
+
+			client.messages.create(body=message , from_="+12055022576" , to = c.phone_number)
+
+
+
+
+
+
+
+
+
+
